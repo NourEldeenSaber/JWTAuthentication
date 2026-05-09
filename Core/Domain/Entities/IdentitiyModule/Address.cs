f98c88bc41b2badf5c6def4d234b2d43f62c6d64
NourSaber <nour01saber@gmail.com>
2026-05-09 14:30:45 +0300

    feat(identity): add ApplicationUser and Address entities

---
+++ Core/Domain/Entities/IdentitiyModule/Address.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain.Entities.IdentitiyModule
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string City { get; set; } = default!;
+        public string Street { get; set; } = default!;
+        public string Country { get; set; } = default!;
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+
+        #region Address [1] => User[1] : Mandatory
+        public ApplicationUser User { get; set; } = default!;
+        public string UserId { get; set; } = default!;
+
+        #endregion
+    }
+}
